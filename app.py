b8751615ea97fc73c6547fedbebbe082f5e1f1de
Daniel Yudi de Carvalho <daniel.carvalho@compasso.com.br>
2024-11-20 10:09:13 -0400

    feat: prepare to deploy run server path

--- app.py
+++ app.py
@@ -593,7 +593,8 @@ def main():
     app = build_dashboard(df_processed)
 
     # Run the app
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
+
 
 if __name__ == '__main__':
     main()
